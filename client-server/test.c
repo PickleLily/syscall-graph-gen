39e7c05056835cf9724f02cc907e579c40a62f0b
PickleLily <lily.bromberger@gmail.com>
2024-11-19 12:49:39 -0500

    attempting to read and write to and from a file...

--- client-server/test.c
+++ client-server/test.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
+// #include <sys/socket.h>
 
 int main(int argc, char* argv[]){
     if (argc < 2){
@@ -13,44 +13,78 @@ int main(int argc, char* argv[]){
     int arg = atoi(argv[1]);
     
     if(arg == 1){
-        printf("specified option 1");
-    }
+        printf("specified option 1\n");
 
-    else if(arg == 2){
-        printf("specified option 2");
-        //send http request to https://icanhazdadjoke.com/api
+        // open file
+        FILE *f = fopen("./file.txt", "rw");
+        // read contents
+        if(f == NULL){
+            printf("file open failure...\n");
+            return 0;
+        }
+            char buff1[10];
+            fgets(buff1, 1,f);
+            // int n = getw(f);
+            int n = atoi(buff1);
+            printf("number: %d \n", n);
+            int new_n = n + 1;
+            char int_ptr[10];
 
-        //create socket to send http message
-        int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+            sprintf(int_ptr, "%d", new_n);
+            printf("new number: %d \n", new_n);
 
-        //load this custom string into the message variable
-        char message[1024];
-        char path[1024] = 'https://icanhazdadjoke.com/j/';
+            // fprintf(f, "%d", new_n);
+            //fputs(int_ptr, f);
+            putw(new_n, f);
+            char buff[10];
+            fgets(buff, 1,f);
+            int val = atoi(buff);
+            printf("new number: %d \n", val);
 
-        //generate random number
-        srand(42);
-        int random_number = (rand() % 100);
-        char random_number_string[10];
-        sprintf(random_number_string, "%d", random_number);
+        // increment number
 
-        //generate path with random joke ID number
-        strcat(path, random_number_string);
+        // write to file
+
+        // close file
+
+        // return number read
+    
+    }else if(arg == 2){
+        // printf("specified option 2\n");
+        // //send http request to https://icanhazdadjoke.com/api
+
+        // //create socket to send http message
+        // int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        // connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+
+        // //load this custom string into the message variable
+        // char message[1024];
+        // char path[1024] = 'https://icanhazdadjoke.com/j/';
+
+        // //generate random number
+        // srand(42);
+        // int random_number = (rand() % 100);
+        // char random_number_string[10];
+        // sprintf(random_number_string, "%d", random_number);
+
+        // //generate path with random joke ID number
+        // strcat(path, random_number_string);
         
-        //create HTTP req.
-        sprintf(message, "GET %s HTTP/1.1\r\n\r\n", path);
+        // //create HTTP req.
+        // sprintf(message, "GET %s HTTP/1.1\r\n\r\n", path);
 
-        //send HTTP message
-        send(sock, message, strlen(message), 0);
+        // //send HTTP message
+        // send(sock, message, strlen(message), 0);
 
     }
     else if(arg == 3){
+        printf("specified option 3\n");
         time_t t;
         time(&t);
         printf("current time:%s", ctime(&t));
         return 0;  
     }else{
-        printf("Specified an option that was not available");
+        printf("Specified an option that was not available\n");
         return -1;
     }  
 }
