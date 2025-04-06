08c409edea03f50966192fb1a6c123cd4fabd880
DunneyBunny <esdunne@wpi.edu>
2025-04-06 19:14:32 -0400

    Graph and Dot File generation changes.
    This also includes a change in previous commit which adds timestamps as the naming convention.

--- Final Graph Work/PNG_Generation.c
+++ Final Graph Work/PNG_Generation.c
@@ -13,14 +13,21 @@ int main(int argc, char *argv[]){
     for(int i = 1; i < argc; i++){
         // Build the dot and png file for each
         int argLen = strnlen(argv[i], MAX_LEN)+1; // Have to re-add NULL terminator
-        char dot[argLen+5];
-        char png[argLen+5];
+        char dot[argLen+4+14+4];
 
-        // Fill dot and png file details
-        strncpy(dot, argv[i], argLen);
+
+        // Build dot file string
+        strncpy(dot, "\"", 2); // Place starting "
+        strncat(dot, ".\\Dot Files\\", 14); // Add file path name
+        strncat(dot, argv[i], argLen); // Add actual file name
+        strncat(dot, ".dot", argLen+4); // Add .dot to end
+        strncat(dot, "\"", 2); // Add closing "
+        printf("%s\n", dot); // Print if neede dfor debugging
+
+        // Build png file name
+        char png[argLen+5];
         strncpy(png, argv[i], argLen);
-        strncat(dot, ".dot", argLen+5);
-        strncat(png, ".png", argLen+5);
+        strncat(png, ".png", argLen+4);
 
         // Build and execute command
         char command[256];
@@ -35,10 +42,15 @@ int main(int argc, char *argv[]){
 
         // Move to graphs folder
         char moveCommand[256];
-        sprintf(moveCommand, "mv %s ./Graphs", png);
+        char otherMoveCommand[256];
+        sprintf(moveCommand, "mv .\\%s ./Graphs", png);
         // Give user feedback on if command was successful
         if(system(moveCommand) == -1){
-            perror("Could not execute move");
+            // Try Windows version
+            sprintf(otherMoveCommand, "move %s ./Graphs", png);
+            if(system(otherMoveCommand) == -1){
+                perror("Could not execute move");
+            }
         } else {
             printf("Moved file %s  to 'Graphs' successfully\n", argv[i]);
         }
