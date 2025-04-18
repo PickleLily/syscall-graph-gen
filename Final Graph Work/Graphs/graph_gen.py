8d85013017405f8ec39e10d286d9753bf94aa935
PickleLily <lily.bromberger@gmail.com>
2025-04-18 18:18:09 -0400

    initialization

---
+++ Final Graph Work/Graphs/graph_gen.py
@@ -0,0 +1,13 @@
+# import ... as ...
+
+# Global variables
+MAX_SUBNODES = 100
+MAX_SUBEDGES = 1000
+MAX_SUBGRAPHS = 100
+DOT_TYPE =  "individual"
+DEBUG_LEVEL = 0
+
+# Global graph Reference
+graphs = [MAX_SUBGRAPHS];
+totalGraphs = -1;
+currentGraph = -1;
