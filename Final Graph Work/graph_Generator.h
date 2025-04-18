75ba4ed101e5a2a933f6d4d52d29e0c7ab3c4836
DunneyBunny <esdunne@wpi.edu>
2025-04-18 06:45:51 -0400

    Lots of changes

--- Final Graph Work/graph_Generator.h
+++ Final Graph Work/graph_Generator.h
@@ -8,6 +8,8 @@ typedef struct Node {
     int fd;               // The file descriptor
     char shape[128];
     int nodeID;
+    int nodePID;
+    char process[2];        // Is this node a process or just an FD
 } Node;
 
 typedef struct Edge {
@@ -22,7 +24,7 @@ typedef struct Edge {
 typedef struct Subgraph {
     int graphNum;           // Unique subgraph number
     int isValid;            // Marker to delimit if the graph is valid (editable, or has a complete connection)
-    int currentfd;          // fd of the root accept4 node - not sure if we even need - shpuld this be current fd?
+    int currentfd[20];          // Currently active file descriptors -> We should not need more than 10
     int masterPID;          // The Node ID of the process that starts interactions?
     int masterRemote;       // Master Remote Connector
     Node* nodes[100];       // Hardcoded here
@@ -33,21 +35,21 @@ typedef struct Subgraph {
 
 // Functions
 void* createStruct(size_t structSize);
-Node* createNode(char* args, int fd, char* shape, int nodeID, Subgraph* subgraph);
-Edge* createEdge(int to, int from, char* syscall, char* edgeType, Subgraph* subgraph);
+Node* createNode(char *args, int fd, char *shape, int nodeID, int nodePID, char *process, Subgraph* subgraph);
+Edge* createEdge(int to, int from, char *syscall, char *edgeType, Subgraph* subgraph);
 Subgraph* initializeSubgraph(int fd, char *PID);
 void addEdge(int from, int to, char *syscall);
 void updateEdge(Subgraph* subgraph, int edge, char *newcall, char *edge_type);
-int findOrAddNode(int fileDescriptor, char *args, char PID[], char shape[]);
-int getSubgraphFD(int currentFD);
-int getNodeFD(int currentFD);
+int findOrAddNode(int fileDescriptor, char *args, char PID[], char *process, char shape[]);
+int getSubgraphFD(int currentFD, int currentPID);
+int getProcessNode(int currentFD, int currentPID);
+int getNodeFD(int currentFD, char *args, int currentPID);
 void parseArgs(const char *args, char *output);
 bool parseLine(char line[], int *FD, char *syscall, char *args, char *ret, char *PID);
 bool parseSyscall(char syscall[], char returnValues[], char arguments[], char FD[]);
 void makeSubgraph(int fd, char *remoteVal, char *localVal, char *PID);
-int getSubgraphFD(int currentFD);
 void printSubgraphMetadata();
-void createDOT(char* setting);
+void createDOT(char *setting);
 
 // Retired functions
 // int formatFD(char *fdString);
